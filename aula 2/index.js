5239f38a89ef48a20972cc7dd11eded8e6d3538f
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2022-09-08 21:01:05 -0100

    first commit

---
+++ aula 2/index.js
@@ -0,0 +1,41 @@
+fetch("https://api.frankfurter.app/currencies").then((data) => data.json()).then((data) => {
+  console.log(data)
+
+    currencies_labels(data);
+  });
+function currencies_labels(data){
+    const select_l = document.getElementById("select_l");
+    const select_r = document.getElementById("select_r");
+    const entries = Object.entries(data);
+    for(var i = 0 ; entries.length > i; i++){
+      select_l.innerHTML += `<option value=${entries[i][0]}>${entries[i][1]}</option>` 
+      select_r.innerHTML += `<option value=${entries[i][0]}>${entries[i][1]}</option>` 
+    }
+}
+function converter(currency,to_currency,value){
+  const host = "api.frankfurter.app"
+  fetch(`https://${host}/latest?amount=${value}&from=${currency}&to=${to_currency}`)
+  .then(res => res.json())
+  .then(data =>{
+    var input_readonly = document.getElementById("input_readonly")
+    input_readonly.value = data.rates[to_currency]
+  } )
+
+}
+
+function currency_convertor(){
+  var input_currency = document.getElementById("input_currency")
+  var erro_satatus = document.getElementById("erro_satatus")
+  var select_currency = document.getElementById("select_l").value
+  var select_to_currency = document.getElementById("select_r").value
+
+  if (select_currency != select_to_currency){
+    var value = converter(select_currency,select_to_currency,input_currency.value)
+  }
+  else{
+    erro_satatus.innerHTML = "Select two different currencies"
+  }
+
+
+
+}
