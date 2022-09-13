f8571bf453cbc4d6785362fb31f648f9ecafcdf6
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2022-09-13 22:00:10 -0100

    feat: conexão front end e back end volleyballscoreboards

--- VolleyballScoreboards- aula 6/Front-End/index.js
+++ VolleyballScoreboards- aula 6/Front-End/index.js
@@ -1,11 +1,87 @@
-const hello_text = document.getElementById("Hello_text");
+const table_body = document.getElementById("table_body");
+const api_url = "http://127.0.0.1:3000"
 
+function CreateTeamRow(team){
+    var table_row = document.createElement('TR');
+    var team_name = document.createElement('TD');
+    var games = document.createElement('TD');
+    var victories = document.createElement('TD');
+    var defeats = document.createElement('TD');
+    var score = document.createElement('TD');
 
-async function get_text(){
-    const response =  await fetch("http://127.0.0.1:3000")
-    const data = await response.json()
-    hello_text.innerText = data.data
-    
+    table_row.id = team.id
+    team_name.innerText = team.team_name;
+    games.innerText = team.games;
+    victories.innerText = team.victories;
+    defeats.innerText = team.defeats;
+    score.innerText = team.score;
+
+    table_row.append(team_name);
+    table_row.append(games);
+    table_row.append(victories);
+    table_row.append(defeats);
+    table_row.append(score);
+
+    return table_row;
 }
 
-get_text()
+function ShowTeams(team_list){
+    for(let i=0 ; i <team_list.length; i++){
+        table_row = CreateTeamRow(team_list[i])
+        console.log(table_row)
+        table_body.append(table_row)
+    }
+}
+
+async function get_all_team(){
+    let url = api_url + "/teams"
+    const response =  await fetch(url)
+    const data = await response.json()
+    ShowTeams(data)
+}
+
+get_all_team()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// async function get_text(){
+//     const response =  await fetch(api_url)
+//     const data = await response.json()
+//     hello_text.innerText = data.data
+    
+// }
+
+
+
